55accf16abba8fd5a95fd045f95ee4c74bb45ce9
Florian Fittschen <ffittschen@gmail.com>
2016-11-12 14:16:27 +0100

    Calculate the top constraint of the StackView on the Home Tab

--- HackaTUM/HomeTabViewController.swift
+++ HackaTUM/HomeTabViewController.swift
@@ -20,6 +20,7 @@ class HomeTabViewController: UIViewController {
     @IBOutlet weak var funBucks: UILabel!
     @IBOutlet weak var redeemButton: UIButton!
     @IBOutlet weak var giftButton: UIButton!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
     fileprivate let disposeBag: DisposeBag
     fileprivate let viewModel: HomeTabViewModel
@@ -39,6 +40,14 @@ class HomeTabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Calculate the top constraint, using the statusBarHeight and the navigationBarHeight
+        topConstraint.constant = UIApplication.shared.statusBarFrame.height + 8
+        
+        if let height = navigationController?.navigationBar.bounds.height {
+            topConstraint.constant = topConstraint.constant + height
+        }
+        
+        // RxSwift
         viewModel.funBucks
             .map { intValue -> String? in
                 return "\(intValue)"
